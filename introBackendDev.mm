5248058e15ae3251e789ce96ba23cbba0719001b
minxola <reminxola69@gmail.com>
2021-11-16 00:22:48 -0500

    HTTP, el lenguaje que habla Internet

---
+++ introBackendDev.mm
@@ -0,0 +1,137 @@
+# Curso de Introducción al Desarrollo Backend
+
+---
+
+Por: Facundo García Martoni
+
+Platzi 2021
+
+Autor: REM - Notas del Curso
+
+---
+
+## 1. Lo que se aprenderá
+
+- Bases para ser desarrollador Backend
+- Diferencia entre Framework y libreria
+- Protocolos HTTPS
+- Servidores y tipos de servidor
+- Flujos de trabajo
+- API y JSON
+- Desarrolo de una API
+
+### Requisitos 
+
+El requisito recomendado es el Curso Básico de Python.
+
+## 2. Yin y Yang de una aplicación: frontend y backend
+
+Las aplicaciones tienen una interfaz y una lógica que hace que funcione. La interfaz se le llama Frontend y la lógica es el Backend.
+
+El Frontend y el Backend se pueden construir con diversas tecnologías.
+
+### Frontend
+
+Las teconologías usadas para el Frontend:
+
+- **HTML**, para la creación de la estructura
+- **CSS**, para dar estilos
+- **JS**, para agregar funcionalidad e interacción
+
+#### Librerias y Frameworks
+
+Para CSS tenemos:
+
+- Foundation
+- Bootstrap
+- Tailwind
+
+Que nos ayudan a escribir código de manera mas rápida y sencilla.
+
+Para JAVASCRIPT tenemos:
+
+- REACT.JS
+- ANGULAR
+- SVELTE
+- VUE
+
+Nos permitén escribir código de manera fácil, rápida y segura para tener menos errores en el frontend de una aplicación.
+
+Antes de empezar a crear una aplicación con estas tecnológias hay una etapa de diseño, sin esta etapa obtendríamos una aplicación poco estética.
+
+#### Diseño de una aplicación
+
+A esta tarea se dedican los diseñadores donde encontramos dos ramas importantes **UI DESIGN** y **UX DESIGN**, que se complementan.
+
+Para UI Design tenemos algunas aplicaciones como:
+
+- Adobe XD
+- Sketch
+- Figma
+
+### Backend
+
+Para el backend tenemos gran cantidad de lenguajes:
+
+- JavaScript, con Node.JS
+- PHP, con Laravel
+- Java, con Spring
+- Go
+- Rust
+- Ruby, con Ruby On Rails
+- Python, con Fast API, Flask y DJango
+
+## 3. Framework y Librería
+
+Aunque parezcan lo mismo, no lo son.
+
+### Framework
+
+Es un conjunto de herramientas (librerías, reglas, estandares) que trabajan en un proyecto complejo bajo ciertas reglas.
+
+- Tiene funcionalidades integradas para evitar el uso de librerías externas.
+- La compatibilidad de sus funcionalidades esta asegurada.
+- El *Framework define* la forma en que debes desarrollar el proyecto.
+
+### Librería
+
+Es una herramienta con una sola utilidad específica.
+
+- El desarrollador es libre de usar la librería en la estructura que desee.
+- Se debe controlar la compatibilidad de una librería con las demás.
+- Se puede usar varias librerías según las necesidades.
+
+## 4. Conectando Frontend y Backend: API y JSON
+
+El frontend y el backend se unen mediante un componente especial llamado **API**, cuyas siglas provienen del inglés Application Program Interface.
+
+Existen dos estandares para las API's:
+
+- SOAP: Simple Object Access Protocol
+
+  Utiliza Extensible Markup Language o XML para enviar la información. Ha caido en desuso debido a que existe una mejor tecnología, muy superior y mas eficiente llamada REST.
+
+- REST: Representational State Transfer
+
+  Utiliza JavaScript Object Notation o JSON para el intercambio de información entre el frontend y backend. Los diccionarios de Python y los Objetos de JavaScrips son similares.
+
+## 5. HTTP, el lenguaje qeu habla Internet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
